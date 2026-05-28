0447eacf32c737e6267a219d5314fc420dd887f2
Eric Otuvaka <eric.otuvaka@gmail.com>
2026-05-27 20:34:43 -0700

    wip: start of concurrent cache service

--- cache/src/main.rs
+++ cache/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let stream = listener.accept().await;
+
+        match stream {
+            Ok((mut stream, _)) => {
+                println!("accepted new connection");
+
+                tokio::spawn(async move {
+                    let mut buf = [0; 512];
+                    loop {
+                        let read_count = stream.read(&mut buf).await.unwrap();
+                        if read_count == 0 {
+                            break;
+                        }
+
+                        stream.write(b"PONG\r\n").await.unwrap();
+                    }
+                });
+            }
+            Err(e) => {
+                println!("error: {}", e)
+            }
+        }
+    }
 }
