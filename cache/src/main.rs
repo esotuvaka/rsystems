bcea94312bf823b8d1e855d347b29f029b64f015
eric otuvaka <eric.otuvaka@gmail.com>
2026-06-08 20:04:14 -0700

    wip: manual impl of cache

--- cache/src/main.rs
+++ cache/src/main.rs
@@ -1,19 +1,60 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
+use tokio::sync::Mutex;
 
 mod parser;
 mod protocol;
 mod store;
 
-use protocol::{Command, Response};
-use store::CacheStore;
+// Alias for int type to differentiate
+type Byte = u8;
+
+// Allows us to implement custom methods over the type like scans, windows, RLE, etc.
+type Bytes = Vec<Byte>;
+
+enum CacheErr {
+    Network(String),
+    Request(String),
+    Response(String),
+}
+
+struct Store {
+    // TODO: convert from storing KV as string:string into bytes:bytes
+    str_items: Arc<Mutex<HashMap<String, String>>>,
+    // PERF: may be able to use String -> RLE -> bytes for store size
+    byt_items: Arc<Mutex<HashMap<String, Bytes>>>,
+    // Space for metadata, global storage settings, etc
+}
+
+impl Store {
+    fn new() -> Self {
+        Self {
+            str_items: Arc::new(Mutex::new(HashMap::new())),
+            byt_items: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    // Explicitly show 3 end states; value either
+    // - exists
+    // - does not exist
+    // - retrieval errors
+    async fn get(self, key: &str) -> Result<Option<String>, CacheErr> {
+        match self.str_items.lock_owned().await.get(key) {
+            Some(v) => Ok(Some(v.to_owned())),
+            None => Ok(None),
+        }
+    }
+
+    async fn set(self, key: &str, val: &str, exp_ms: Option<i32>) -> Result<(), CacheErr> {}
+}
 
 #[tokio::main]
 async fn main() {
     // Clone store for use in spawned tasks
-    let cache_store = Arc::new(CacheStore::new());
+    let cache_store = Arc::new(Store::new());
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
@@ -50,7 +91,7 @@ async fn main() {
 }
 
 /// Process incoming command and generate response
-async fn process_command(store: &CacheStore, data: &[u8]) -> String {
+async fn process_command(store: &Store, data: &[u8]) -> String {
     let input = String::from_utf8_lossy(data).to_string();
     let command = parser::parse(&input).await;
 
--- cache/src/protocol/mod.rs
+++ cache/src/protocol/mod.rs
@@ -1,2 +1,1 @@
-mod command;
-pub use command::{Command, Response};
+
